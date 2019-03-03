ebcb64c484b811974ad2c474cee5d5f52f2e84e1
阿小庆 <1756260160@qq.com>
2019-03-03 18:15:14 +0800

    print

---
+++ python3_tutorials_莫凡/02print().py
@@ -0,0 +1,20 @@
+>> > print(we are going to do sth)
+  File "<stdin>", line 1
+    print(we are going to do sth)
+               ^
+SyntaxError: invalid syntax
+>> > print('we are going to do sth')
+we are going to do sth
+>> > print("!I'm cq")
+!I'm cq
+>> > print('apple' + 'eat')
+appleeat
+>> > print('apple' + '4')
+apple4
+>> > print('apple' + str(4))
+apple4
+>> > print(int('1')+2)
+3
+>> > print(complex(1))
+(1+0j)
+>> >
