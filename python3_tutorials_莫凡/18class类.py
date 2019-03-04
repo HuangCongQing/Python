dd621e924ff67731167dd69c701a438edbac64b0
阿小庆 <1756260160@qq.com>
2019-03-04 22:47:11 +0800

    add class类

---
+++ python3_tutorials_莫凡/18class类.py
@@ -0,0 +1,25 @@
+# class A开头大写
+
+class Calculator:
+    name = "Good calculator"
+    price= 10000
+    def add(self, x, y):  # 默认self
+        print(self.name)
+        result = x + y
+        print(result)
+    def minus(self, x, y):
+        result = x + y
+        print(result)
+    def times(self, x, y):
+        result = x * y
+        print(result)
+    def divide(self, x, y):
+        print()
+
+# 调用
+
+cal = Calculator()
+cal.name
+cal.price  # 为什么没有输出？？？？？？？？？
+cal.add(10,11)
+    
