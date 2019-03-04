3e1b8d6f29681ec26d062986da2d11fca4acfd5a
阿小庆 <1756260160@qq.com>
2019-03-04 22:23:55 +0800

    add 15 append

---
+++ python3_tutorials_莫凡/15读写文件2.py
@@ -0,0 +1,10 @@
+# text = "This is my first line.\nThis is my next line."
+# print(text)
+append_text="\nThis is a appended file"
+
+my_file = open('my file.txt', 'a')
+my_file.write(append_text)
+my_file.close()
+
+
+# append文件
