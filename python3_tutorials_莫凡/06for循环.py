6e04d5b30564bc94cc9a87ce7f0b592237246967
阿小庆 <1756260160@qq.com>
2019-03-04 16:35:34 +0800

    add:for循环

---
+++ python3_tutorials_莫凡/06for循环.py
@@ -0,0 +1,16 @@
+''' example_list = [1, 3, 5, 7, 3, 7, 8, 2, 5, 7, 3563, 36446, 635] #列表 可改变
+for i in example_list:
+    print(i)
+
+print("outer of for") '''
+
+# 内部函数
+# range(1,3) [1,2]
+for i in range(1, 10, 2):
+    print(i)
+
+# 1
+# 3
+# 5
+# 7
+# 9
