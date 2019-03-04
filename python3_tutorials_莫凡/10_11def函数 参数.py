90018d9330bfc79e1c47fc5f29887a3025b05757
阿小庆 <1756260160@qq.com>
2019-03-04 17:20:33 +0800

    add：函数

---
+++ python3_tutorials_莫凡/10_11def函数 参数.py
@@ -0,0 +1,13 @@
+# def函数
+
+
+a = 1
+b = 2
+c = a + b
+print(c)
+
+def function(a, b):
+    c = a + b
+    print('this c is   ', c)
+
+function(1,2)
---
+++ python3_tutorials_莫凡/12函数默认参数.py
@@ -0,0 +1,11 @@
+def sale_car(price, color='red', brand='carmy', is_second_hand='True'):
+    print('price:', price,
+        'colour', color,
+        'brand', brand,
+        'is_second', brand,
+    )
+
+# sale_car(1000,'red','carmy', 'True')
+sale_car(1001212, 'brue')
+
+# 默认值后面必须是默认值
