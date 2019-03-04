5d19bda0306a283fcdb399c94ad922126d78183f
阿小庆 <1756260160@qq.com>
2019-03-04 16:51:57 +0800

    add: 判断语句

---
+++ python3_tutorials_莫凡/07_09if条件.py
@@ -0,0 +1,26 @@
+x = 1
+y = 2
+z = 3
+
+#07 #08if else
+
+# if x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is less than y")
+
+# m = 1
+# n = 1
+# if m == n:
+#     print("m is equal to n")
+
+
+# 09 if: elif: else:
+
+
+if x < y:
+    print("x is less than y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is greater than y")
