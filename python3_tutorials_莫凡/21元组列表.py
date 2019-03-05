7fe5e4f901682564b6cda4ec4c2781b768f6eb40
阿小庆 <1756260160@qq.com>
2019-03-05 16:59:27 +0800

    add 元组列表

---
+++ python3_tutorials_莫凡/21元组列表.py
@@ -0,0 +1,11 @@
+# tuple list
+
+a_tuple = (1, 2, 3)
+another_tuple = 1,2,3
+a_list = [4, 5, 6]
+
+# for content in a_list:
+#     print(content)
+
+for index in range(len(a_list)):
+    print("index=",index,"number in list=", a_list[index])
