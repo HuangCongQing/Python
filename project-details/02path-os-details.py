26683bf185c4906f07e2e86a83eaaf916351aa14
阿小庆 <1756260160@qq.com>
2020-07-21 09:18:54 +0800

    02path-os-details

--- project-details/02path-os-details.py
+++ project-details/02path-os-details.py
@@ -4,7 +4,7 @@
 @Company(School): UCAS
 @Date: 2020-07-20 22:30:12
 @LastEditors: HCQ
-@LastEditTime: 2020-07-20 22:41:05
+@LastEditTime: 2020-07-21 09:18:04
 '''
 # 参考：记录下os.path.dirname(__file__)使用 https://blog.csdn.net/JOJOY_tester/article/details/54598713
 
@@ -12,6 +12,7 @@
 # os.path.abspath(__file__)返回的是.py文件的绝对路径
 
 import os
+import sys
 #该文件所在位置：D:\第1层\第2层\第3层\第4层\第5层\test11.py
  
 path1 = os.path.dirname(__file__)
@@ -33,3 +34,16 @@ path6 = os.__file__                  #获取os所在的目录
 print(path6)  # D:\ProgramData\Anaconda3\lib\os.py
 path7 = os.path.abspath(__file__)   # 返回的是.py文件的绝对路径。
 print(path7)  # 
+
+# 项目中
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+print("sys.path: ", sys.path) # sys.path是一个数组
+
+DATA_DIR = os.path.join(BASE_DIR, 'data') # 在本目录新建data文件夹
+print("DATA_DIR \n", DATA_DIR)
+www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+zipfile = os.path.basename(www) # 获得文件名modelnet40_ply_hdf5_2048
+print('zipfile', zipfile)
+# os.system 运行linux命令
+os.system('wget --no-check-certificate %s; unzip %s' % (www, zipfile))
