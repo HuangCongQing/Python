78e1c907c5b7a834c4b40fdc49ca331639ed5241
HuangCongQing <1756260160@qq.com>
2020-11-17 14:05:28 +0800

    add: 测试

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+Description: test
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2020-11-17 13:50:28
+LastEditTime: 2020-11-17 14:02:14
+FilePath: /Python/test.py
+'''
+import socket
+
+print(socket.gethostname())  # hcq-G5-5590
