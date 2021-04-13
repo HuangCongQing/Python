430abb8bcb172788cb9eae82554f542eeca98645
HuangCongQing <1756260160@qq.com>
2021-04-13 17:29:40 +0800

    update:txt转换pkl

---
+++ data_process/txt/txt_process.py
@@ -0,0 +1,9 @@
+'''
+Description: 
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2021-04-13 17:29:15
+LastEditTime: 2021-04-13 17:29:16
+FilePath: /Python/data_process/txt/txt_process.py
+'''
