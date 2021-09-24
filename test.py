fbd3ba685cc81a9dab41dd170725b3ae382c498d
HuangCongQing <1756260160@qq.com>
2021-09-24 15:37:38 +0800

    add

--- test.py
+++ test.py
@@ -10,3 +10,12 @@ FilePath: /Python/test.py
 import socket
 
 print(socket.gethostname())  # hcq-G5-5590
+
+import numpy as np
+
+a = np.arange(12).reshape(1,3,4)
+print(a)
+# print(a>4)
+# print(a[a>4])
+print(a[:, :, 3]>8)
+print(a[a[:, :, 3]>8])
