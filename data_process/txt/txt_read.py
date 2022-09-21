4cde44f76767c90f1693ec03d0eab615586d0c50
chongqinghuang <chongqinghuang@autox.ai>
2022-09-21 17:42:42 +0800

    add

--- data_process/txt/txt_read.py
+++ data_process/txt/txt_read.py
@@ -4,11 +4,11 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-04-13 18:12:20
-LastEditTime: 2022-09-21 17:40:23
+LastEditTime: 2022-09-21 17:42:33
 FilePath: /Python/data_process/txt/txt_read.py
 '''
 
-with open('../extracted/'+file, 'w') as p:
+with open('../extracted/'+file, 'r') as p:
     count = 0
     for line in f.readlines():
         if '>' == line[0]:
